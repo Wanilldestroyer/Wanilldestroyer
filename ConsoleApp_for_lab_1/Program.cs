9808a96a2a1d372b927e420b11f073140cdf5bbf
Wan <60813642+Wanilldestroyer@users.noreply.github.com>
2024-04-30 14:45:18 +0300

    Add files via upload
    For lab_rab that include Csharp language

---
+++ ConsoleApp_for_lab_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите массив из нулей и единиц (без пробелов):");
+        string binaryInput = Console.ReadLine();
+
+        try
+        {
+            int decimalNumber = Convert.ToInt32(binaryInput, 2);
+
+            if (decimalNumber >= 0 && decimalNumber <= 100)
+                Console.WriteLine("Число в диапазоне от 0 до 100." + decimalNumber);
+            else
+                Console.WriteLine("Число вне диапазона от 0 до 100." + decimalNumber);
+
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Неправильный ввод. Введите только нули и единицы без пробелов.");
+        }
+    }
+}
